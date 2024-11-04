904e53f78b7c78d5fa4dc593c0f9fcad772b6689
Filippo Gaggi <filippo.gaggi@studio.unibo.it>
2024-11-04 12:00:47 +0100

    Exercise done until point 3

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,44 @@
+package it.unibo.inner.impl;
+
+import java.util.Iterator;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+
+    private T[] list;
+
+    public IterableWithPolicyImpl(final T[] list) {
+        this.list = list;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        IteratorImpl test = new IteratorImpl();
+        return test;
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+    }
+
+    private class IteratorImpl implements Iterator<T> {
+
+        private int current;
+
+        @Override
+        public boolean hasNext() {
+            return (current <= list.length);
+        }
+        @Override
+        public T next() {
+            this.current++;
+            return list[this.current];
+        }
+        
+    }
+    
+}
